93c118f38b85b7c90ad0be3139a0bd20bb902a27
AkankshaRaj2002 <akankshapandit039@gmail.com>
2024-01-15 17:57:16 +0530

    all changes mage till now

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET"])
 
 def welcome():
     return render_template('home.html')
 
-@app.route("/")
+@app.route("/",methods=["PUSH"])
 
 def symptoms():
     return render_template('symptoms.html')
